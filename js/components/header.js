775ebaef9aa0b963df2ce05dedd2970f9cf33a8e
Gabriel Valeze Elisbão do Nascimento <gabrielvaleze16@gmail.com>
2025-12-19 20:09:21 -0300

    Add loadHeader function to render header HTML

---
+++ js/components/header.js
@@ -0,0 +1,11 @@
+export function loadHeader() {
+  document.getElementById("header").innerHTML = `
+    <header class="header">
+      <h2>Chess Academy</h2>
+      <nav>
+        <a href="index.html">Home</a>
+        <a href="tactics-course.html">Cursos</a>
+      </nav>
+    </header>
+  `;
+}
