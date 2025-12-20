620857ed3e0425cc60e77441a3e0a195d9031e67
Gabriel Valeze Elisbão do Nascimento <gabrielvaleze16@gmail.com>
2025-12-19 21:54:25 -0300

    Update header.js

--- js/components/header.js
+++ js/components/header.js
@@ -1,10 +1,12 @@
 export function loadHeader() {
   document.getElementById("header").innerHTML = `
     <header class="header">
-      <h2>Chess Academy</h2>
-      <nav>
+      <h2 class="logo">Chess Academy</h2>
+
+      <nav class="nav">
         <a href="index.html">Home</a>
         <a href="tactics-course.html">Cursos</a>
+        <a href="login.html" class="btn-login">Entrar</a>
       </nav>
     </header>
   `;
